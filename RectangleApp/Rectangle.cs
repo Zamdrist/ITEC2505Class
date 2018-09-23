fc8b311f674845165cfdd469eac4971c7b9b0031
Steve <zamdrist@gmail.com>
2018-09-22 19:53:00 -0500

    Untabify

--- RectangleApp/Rectangle.cs
+++ RectangleApp/Rectangle.cs
@@ -1,17 +1,17 @@
 ﻿namespace RectangleApp
 {
-	public class Rectangle
-	{
+    public class Rectangle
+    {
 
-		public decimal Area { get; private set; }
-		public decimal Perimeter { get; private set; }
+        public decimal Area { get; private set; }
+        public decimal Perimeter { get; private set; }
 
-		public Rectangle(decimal length, decimal width)
-		{
-			this.Area = length * width;
-			this.Perimeter = 2 * (length + width);
+        public Rectangle(decimal length, decimal width)
+        {
+            this.Area = length * width;
+            this.Perimeter = 2 * (length + width);
 
-		}
+        }
     }
 
 
--- RectangleApp/frmRectangle.cs
+++ RectangleApp/frmRectangle.cs
@@ -10,30 +10,30 @@ namespace RectangleApp
             this.InitializeComponent();
         }
 
-		private void btnCalc_Click(object sender, EventArgs e)
-		{
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
             decimal.TryParse(this.txtLength.Text, out var length);
             decimal.TryParse(this.txtWidth.Text, out var width);
 
-			var rectangle = new Rectangle(length, width);
+            var rectangle = new Rectangle(length, width);
 
-			this.lblArea.Text = rectangle.Area.ToString();
-			this.lblPerimeter.Text = rectangle.Perimeter.ToString();
+            this.lblArea.Text = rectangle.Area.ToString();
+            this.lblPerimeter.Text = rectangle.Perimeter.ToString();
 
-		}
+        }
 
-		private void btnClear_Click(object sender, EventArgs e)
-		{
-			const string zeroString = "0";
-			this.txtLength.Text = zeroString;
-			this.txtWidth.Text = zeroString;
-			this.lblArea.Text = zeroString;
-			this.lblPerimeter.Text = zeroString;
-		}
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            const string zeroString = "0";
+            this.txtLength.Text = zeroString;
+            this.txtWidth.Text = zeroString;
+            this.lblArea.Text = zeroString;
+            this.lblPerimeter.Text = zeroString;
+        }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-			this.Close();
+            this.Close();
         }
     }
 }
