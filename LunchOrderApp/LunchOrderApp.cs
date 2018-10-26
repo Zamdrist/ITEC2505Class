45514deaae70aab26f9d7f659b5a66e279d87c50
Steve <zamdrist@gmail.com>
2018-10-26 06:15:57 -0500

    Untabify lines

--- LunchOrderApp/LunchOrderApp.cs
+++ LunchOrderApp/LunchOrderApp.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LunchOrderApp
@@ -19,10 +13,10 @@ namespace LunchOrderApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-			var lunch = new Lunch();
-	        this.dgMainCourse.DataSource = lunch.MainCourse;
-	        this.dgMainCourse.Columns[1].DefaultCellStyle.Format = "c2";
-	        this.dgMainCourse.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            var lunch = new Lunch();
+            this.dgMainCourse.DataSource = lunch.MainCourse;
+            this.dgMainCourse.Columns[1].DefaultCellStyle.Format = "c2";
+            this.dgMainCourse.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
 
 
         }
