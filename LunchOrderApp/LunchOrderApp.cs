3244b80d50498c313cac44a560cd439d680dcbad
Steve <zamdrist@gmail.com>
2018-11-03 00:26:09 -0500

    Refactoring to build order summary list

--- LunchOrderApp/LunchOrderApp.cs
+++ LunchOrderApp/LunchOrderApp.cs
@@ -1,106 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
 namespace LunchOrderApp
 {
-    public partial class LunchOrderApp : Form
-    {
-	    private readonly Lunch _lunch = new Lunch();
+	public partial class LunchOrderApp : Form
+	{
+		private readonly Lunch _lunch = new Lunch();
 
+		public LunchOrderApp()
+		{
+			this.InitializeComponent();
 
-        public LunchOrderApp()
-        {
-            this.InitializeComponent();
+		}
 
-        }
+		private void LunchOrderApp_Load(object sender, EventArgs e)
+		{
+			this.dgAddOns.AutoGenerateColumns = false;
+			this.dgOrderSummary.AutoGenerateColumns = false;
+			this.dgMainCourse.AutoGenerateColumns = false;
+			this.dgMainCourse.DataSource = this._lunch.MainCourse;
+			this.dgMainCourse.ClearSelection();
+		}
 
-        private void LunchOrderApp_Load(object sender, EventArgs e)
-        {
-            this.dgMainCourse.DataSource = this._lunch.MainCourse;
-	        this.dgMainCourse.Columns[0].Width = 30;
-	        this.dgMainCourse.Columns[0].HeaderText = string.Empty;
-	        this.dgMainCourse.Columns[1].AutoSizeMode =
-		        DataGridViewAutoSizeColumnMode.Fill;
-            this.dgMainCourse.Columns[2].DefaultCellStyle.Format = "c2";
-	        this.dgMainCourse.ClearSelection();
-	        this.dgOrderSummary.DataSource = null;
-        }
+		private void dgMainCourse_SelectionChanged(object sender, EventArgs e)
+		{
+			if (this.dgMainCourse.Focused)
+			{
+				if (this.dgMainCourse.SelectedRows.Count != 0)
+				{
+					var mainCourseItem = this.dgMainCourse
+						.Rows[this.dgMainCourse.SelectedRows[0].Index].Cells["MainCourseItem"].Value
+						.ToString();
 
-	    private void dgMainCourse_SelectionChanged(object sender, EventArgs e)
-	    {
-		    if (this.dgMainCourse.Focused)
-		    {
-			    if (this.dgMainCourse.SelectedRows.Count != 0)
-			    {
-				    var mainCourseItem = this.dgMainCourse
-					    .Rows[this.dgMainCourse.SelectedRows[0].Index].Cells[1].Value.ToString();
+					Enum.TryParse(mainCourseItem, out Lunch.LunchOrderItem selectedMainCourse);
 
-				    if (Enum.TryParse(mainCourseItem, out Lunch.LunchOrderItem selectedMainCourse))
-				    {
-					    this.dgAddOns.DataSource = this._lunch.AddOn
-						    .Where(i => i.MainCourseItem == selectedMainCourse).ToList();
+					this.dgAddOns.DataSource = this._lunch.AddOn
+						.Where(addOn => addOn.MainCourseItem == selectedMainCourse).ToList();
 
-					    this.dgAddOns.Columns[0].Width = 30;
-					    this.dgAddOns.Columns[0].HeaderText = string.Empty;
-					    this.dgAddOns.Columns[1].Visible = false;
-					    this.dgAddOns.Columns[2].AutoSizeMode =
-						    DataGridViewAutoSizeColumnMode.Fill;
-					    this.dgAddOns.Columns[3].DefaultCellStyle.Format = "c2";
-					    this.dgAddOns.ClearSelection();
+					this.dgAddOns.ClearSelection();
+					this.dgOrderSummary.DataSource = this._lunch.OrderSummary(
+						this.dgMainCourse.SelectedRows[0],
+						this.dgAddOns.SelectedRows);
 
-					    var order = new OrderSummary
-					    {
-						    Item = this.dgMainCourse.Rows[this.dgMainCourse.SelectedRows[0].Index]
-							    .Cells[1].Value.ToString(),
-						    Price = (decimal) this.dgMainCourse
-							    .Rows[this.dgMainCourse.SelectedRows[0].Index].Cells[2].Value
-					    };
+					this.dgOrderSummary.ClearSelection();
+					this.txtSubtotal.Text = this._lunch.SubTotal.ToString("C2");
+				}
+				else
+				{
+					this.dgAddOns.DataSource = null;
+					this.dgOrderSummary.DataSource = null;
+					this.txtSubtotal.Text = string.Empty;
+				}
+			}
+		}
 
-					    var orderSummary = new List<OrderSummary> {order};
-					    this.dgOrderSummary.DataSource = orderSummary;
-
-					    foreach (DataGridViewColumn column in this.dgOrderSummary.Columns)
-					    {
-						    column.Width = this.dgOrderSummary.Width
-							    / this.dgOrderSummary.ColumnCount;
-					    }
-                    }
-			    }
-			    else
-			    {
-				    this.dgAddOns.DataSource = null;
-			    }
-		    }
-	    }
-
-
-
-	    private void dgAddOns_SelectionChanged(object sender, EventArgs e)
-        {
-	        if (this.dgAddOns.Focused)
-	        {
-		        var orderList = (List<OrderSummary>)this.dgOrderSummary.DataSource;
-		        this.dgOrderSummary.DataSource = null;
-		        foreach (DataGridViewRow selectedRow in this.dgAddOns.SelectedRows)
-		        {
-			        var addOn = new OrderSummary
-			        {
-				        Item = selectedRow.Cells[2].Value.ToString(),
-				        Price = (decimal) selectedRow.Cells[3].Value
-			        };
-			        orderList.Add(addOn);
-			        this.dgOrderSummary.DataSource = orderList.Distinct().ToList();
-		        }
-
-		        foreach (DataGridViewColumn column in this.dgOrderSummary.Columns)
-		        {
-			        column.Width = this.dgOrderSummary.Width
-				        / this.dgOrderSummary.ColumnCount;
-		        }
-				this.dgOrderSummary.ClearSelection();
+		private void dgAddOns_SelectionChanged(object sender, EventArgs e)
+		{
+			if (this.dgAddOns.Focused)
+			{
+				this.dgOrderSummary.DataSource = this._lunch.OrderSummary(
+					this.dgMainCourse.SelectedRows[0],
+					this.dgAddOns.SelectedRows);
+				this.txtSubtotal.Text = this._lunch.SubTotal.ToString("C2");
+                this.dgOrderSummary.ClearSelection();
+                this.dgOrderSummary.ClearSelection();
             }
+		}
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+			this.Close();
         }
     }
 }
