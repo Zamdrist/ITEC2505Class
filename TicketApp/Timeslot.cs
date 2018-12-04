75a29a447434022e6369ef68a998e1408733d0f2
Steve <zamdrist@gmail.com>
2018-12-03 23:16:00 -0600

    Adding ticketapp

---
+++ TicketApp/Timeslot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TicketApp
+{
+	public class Timeslot
+	{
+		public int TimeWindow { get; } = 4;
+	    public int Minutes { get; set; }
+		public int Guests { get; set; }
+		public decimal RemainingGuests { get; set;}
+		public DateTime StartTime { get; set; }
+		public DateTime EndTime { get; set; }
+		public int FirstTicketNumber { get; set; }
+
+
+	}
+}
